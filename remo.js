60ae7efc3df5d6979ca134eec7ea3d1cfed41176
tripith <rahultariyal720@gmail.com>
2023-01-17 23:51:01 +0530

    jsmethod

--- remo.js
+++ remo.js
@@ -1,18 +1,58 @@
-//selector 
-//const header = document.getElementById("main-header");
-//header.style.borderBottom = "solid 12px  #000"; 
+var listItem = document.querySelector("#items");
+//parent node 
+//console.log(listItem.parentNode);
+//listItem.parentElement.style.backgroundColor = "red";
+//console.log(listItem.parentNode.parentNode);
+
+//parentElement 
+//behave same as parent node
+console.log(listItem.parentElement);
+listItem.parentElement.style.backgroundColor = "grey";
+console.log(listItem.parentElement.parentElement);
+
+//lastElementChild
+listItem.lastElementChild.textContent = "lastchild";
+
+//lastChild
+console.log(listItem.lastChild);// showing text 
+
+//create child
 
 
-//get element by class name  item = document.querySelector(`.list-group-item`);
+//child node 
+//console.log(listItem.childNodes);
+console.log(listItem.children);
+//acess a specfic child
+console.log(listItem.children[1].textContent = "hello->text 1");
+listItem.children[2].style.backgroundColor = "coral";
 
+//firstChild-->fristElementChild
+console.log(listItem.firstElementChild);
+listItem.firstElementChild.textContent = "hellochild";
 
-var odd = document.querySelectorAll("li:nth-child(odd)");
-var even = document.querySelectorAll("li:nth-child(even)");
-for (var i = 0; i < odd.length; i++){
-    odd[i].style.backgroundColor = "blue";
+// nextsibling 
+//console.log(listItem.nextElementSibling);
 
-}
-for (var i = 0; i < even.length; i++){
-    even[i].style.backgroundColor = "green";
+//pervious sibling
+console.log(listItem.previousElementSibling);
+listItem.parentElement.style.background = "red";
 
-}
+//creat Element
+  // create a div 
+  var newDiv = document.createElement('div');
+  //add class
+  newDiv.className = 'hellod';
+  //add id
+  newDiv.id = 'helloD';
+  //set attributr
+  newDiv.setAttribute('title','hello Div');
+
+  //create text node 
+  var newDivText = document.createTextNode('hello world');
+// add text to div
+  newDiv.appendChild(newDivText);
+  var putdiv = document.querySelector("header .container");
+  var h1 = document.querySelector('header h1');
+  console.log(newDiv);
+  newDiv.style.fontSize = "20px";
+  putdiv.insertBefore(newDiv, h1);
