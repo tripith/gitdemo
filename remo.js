c896ac3241592a41af70a1c6571468a4158876c4
tripith <rahultariyal720@gmail.com>
2023-01-17 20:51:19 +0530

    secondcommit

--- remo.js
+++ remo.js
@@ -1,1 +1,8 @@
-console.log("hello git 1");
+//selector 
+//const header = document.getElementById("main-header");
+//header.style.borderBottom = "solid 12px  #000"; 
+
+
+//get element by class name 
+const item = document.getElementsByClassName("list-group-item");
+console.log(item[1].textContent= "hello again");// inte
