e431d0497520832d2e65c0da9d83fb4a2be2e9ec
tripith <rahultariyal720@gmail.com>
2023-01-17 21:13:20 +0530

    backgrounndadded

--- remo.js
+++ remo.js
@@ -5,4 +5,11 @@
 
 //get element by class name 
 const item = document.getElementsByClassName("list-group-item");
-console.log(item[1].textContent= "hello again");// inte
+
+console.log(item[2]);// 
+item[2].style.backgroundColor = "green";
+item[2].style.fontWeight = "bold";
+
+for (var i = 0; i < item.length; i++){
+    item[i].style.backgroundColor = "grey";
+}
